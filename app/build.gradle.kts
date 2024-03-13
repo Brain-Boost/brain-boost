48cea4756ad5ef8b0dbe0870215c3f761a0496b1
gntsqid <sjlang.805@gmail.com>
2024-03-13 14:36:20 -0700

    Added database code: no errors on testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") // SJL added for Room
 }
 
 android {
@@ -40,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10" // changed 1.5.1 to 1.5.10 SJL
     }
     packaging {
         resources {
@@ -60,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,4 +70,9 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // ROOM
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
--- app/src/main/java/com/example/opennewactivitybutton/MainActivity.kt
+++ app/src/main/java/com/example/opennewactivitybutton/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,12 +24,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.room.Room
+import com.example.opennewactivitybutton.database.AppDatabase
+import com.example.opennewactivitybutton.database.entities.Alarm
 import com.example.opennewactivitybutton.ui.theme.Pink80
-import com.example.opennewactivitybutton.MyContent as MyContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /////////////////////////////////////////////////////////////////
+        // Database Creation
+        val db = Room.databaseBuilder( // instantiate it
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries().build() // allowMainThreadQueries is not recommended for production use
+        val alarmDao = db.AlarmDao()
+
+        // Create a new Alarm object
+        val newAlarm = Alarm(label = "Breakfast", hour=2, minute=0, meridian="PM", status=false)
+        // Insert the Alarm into the database
+        alarmDao.insertAlarm(newAlarm)
+        // Retrieve the Alarm from the database
+        val alarm = alarmDao.getAlarmById(newAlarm.label) // local var
+        /////////////////////////////////////////////////////////////////
+
         setContent {
             // Calling the composable function
             // to display element and its contents
