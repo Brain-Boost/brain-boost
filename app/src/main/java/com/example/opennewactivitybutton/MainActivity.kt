38ce31108479813b658aa35cbc17539abc520d59
Fernando V <fernando.villanueva231@myci.csuci.edu>
2024-03-23 14:21:42 -0700

    Added clock that shows live time that is also a pressable button new

--- app/src/main/java/com/example/opennewactivitybutton/MainActivity.kt
+++ app/src/main/java/com/example/opennewactivitybutton/MainActivity.kt
@@ -7,18 +7,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,21 +18,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.inset
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,56 +33,65 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 import com.example.opennewactivitybutton.MyContent as MyContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.graphics.drawscope.rotate
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+/*
+        /////////////////////////////////////////////////////////////////
+        // Database Creation
+        val db = Room.databaseBuilder( // instantiate it
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries().build() // allowMainThreadQueries is not recommended for production use
+        val alarmDao = db.AlarmDao()
+
+        // Create a new Alarm object
+        val newAlarm = Alarm(label = "Breakfast", hour=2, minute=0, meridian="PM", status=false)
+        // Insert the Alarm into the database
+        alarmDao.insertAlarm(newAlarm)
+        // Retrieve the Alarm from the database
+        val alarm = alarmDao.getAlarmById(newAlarm.label) // local var
+        /////////////////////////////////////////////////////////////////
+*/
         setContent {
             // Calling the composable function
             // to display element and its contents
-
             MainContent()
-
         }
     }
 }
-
 
 // Creating a composable
 // function to display Top Bar
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainContent() {
-    //time for the clock
-    var currentTimeInMs by remember {
-        mutableStateOf(System.currentTimeMillis())
-    }
-
-    LaunchedEffect(key1 = true){
-        while(true){
-            delay(200)
-            currentTimeInMs = System.currentTimeMillis()//clock time
-        }
-    }
-
         Scaffold(
             topBar = { TopAppBar(title = { Text("Brain Boost", color = Color.Black) }) },
-            content = {
-                Column (
-                    modifier = Modifier.padding(16.dp)
-                ){
-                    MyContent()
-                    //Clock(time = { currentTimeInMs }, circleRadius = 150f, outerCircleThickness = 25f)
-                }
-                //MyContent()
-            }
-
+            content = { MyContent() }
         )
-
-    
-
 }
-
 
 // Creating a composable function to
 // create two Images and a spacer between them
@@ -144,6 +133,13 @@ fun MyContent(){
     }
 }
 
+// For displaying preview in
+// the Android Studio IDE emulator
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainContent()
+}
 
 @Composable
 fun Clock(
@@ -157,7 +153,7 @@ fun Clock(
         mutableStateOf(Offset.Zero)
     }
     Box(
-       modifier = modifier
+        modifier = modifier
     ){
         Canvas(
             modifier = Modifier
@@ -165,144 +161,144 @@ fun Clock(
 
         ){
             //val canvasQuadrantSize = size/2f
-           // inset(horizontal = 250f, vertical = 50f) {
+            // inset(horizontal = 250f, vertical = 50f) {
 
-                val width = size.width
-                val height = size.height
-                circleCenter = Offset(x = width / 2f, y = height / 2f)
-                val date = Date(time())
-                val cal = Calendar.getInstance()
-                cal.time = date
-                val hours = cal.get(Calendar.HOUR_OF_DAY)
-                val minutes = cal.get(Calendar.MINUTE)
-                val seconds = cal.get(Calendar.SECOND)
+            val width = size.width
+            val height = size.height
+            circleCenter = Offset(x = width / 2f, y = height / 2f)
+            val date = Date(time())
+            val cal = Calendar.getInstance()
+            cal.time = date
+            val hours = cal.get(Calendar.HOUR_OF_DAY)
+            val minutes = cal.get(Calendar.MINUTE)
+            val seconds = cal.get(Calendar.SECOND)
 
 
 
-                drawCircle(
-                    style = Stroke(
-                        width = outerCircleThickness
-                    ),
-                    brush = Brush.linearGradient(
-                        listOf(
-                            white.copy(0.45f),
-                            darkGray.copy(0.35f)
-                        )
-                    ),
-                    radius = circleRadius + outerCircleThickness / 2f,
-                    center = circleCenter
-                )
-                drawCircle(
-                    brush = Brush.radialGradient(
-                        listOf(
-                            white.copy(0.45f),
-                            darkGray.copy(0.25f)
-                        )
-                    ),
-                    radius = circleRadius,
-                    center = circleCenter
-                )
-                drawCircle(
-                    color = gray,
-                    radius = 15f,
-                    center = circleCenter
-                )
-
-                val littleLineLength = circleRadius * 0.1f
-                val largeLineLength = circleRadius * 0.2f
-                for (i in 0 until 60) {
-                    val angleInDegrees = i * 360f / 60
-                    val angleInRad = angleInDegrees * PI / 180f + PI / 2f
-                    val lineLength = if (i % 5 == 0) largeLineLength else littleLineLength
-                    val lineThickness = if (i % 5 == 0) 5f else 2f
-
-                    val start = Offset(
-                        x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
-                        y = (circleRadius * sin(angleInRad) + circleCenter.y).toFloat()
+            drawCircle(
+                style = Stroke(
+                    width = outerCircleThickness
+                ),
+                brush = Brush.linearGradient(
+                    listOf(
+                        white.copy(0.45f),
+                        darkGray.copy(0.35f)
                     )
-
-                    val end = Offset(
-                        x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
-                        y = (circleRadius * sin(angleInRad) + lineLength + circleCenter.y).toFloat()
+                ),
+                radius = circleRadius + outerCircleThickness / 2f,
+                center = circleCenter
+            )
+            drawCircle(
+                brush = Brush.radialGradient(
+                    listOf(
+                        white.copy(0.45f),
+                        darkGray.copy(0.25f)
                     )
-                    rotate(
-                        angleInDegrees + 180,
-                        pivot = start
-                    ) {
-                        drawLine(
-                            color = gray,
-                            start = start,
-                            end = end,
-                            strokeWidth = lineThickness.dp.toPx()
-                        )
+                ),
+                radius = circleRadius,
+                center = circleCenter
+            )
+            drawCircle(
+                color = gray,
+                radius = 15f,
+                center = circleCenter
+            )
+
+            val littleLineLength = circleRadius * 0.1f
+            val largeLineLength = circleRadius * 0.2f
+            for (i in 0 until 60) {
+                val angleInDegrees = i * 360f / 60
+                val angleInRad = angleInDegrees * PI / 180f + PI / 2f
+                val lineLength = if (i % 5 == 0) largeLineLength else littleLineLength
+                val lineThickness = if (i % 5 == 0) 5f else 2f
+
+                val start = Offset(
+                    x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
+                    y = (circleRadius * sin(angleInRad) + circleCenter.y).toFloat()
+                )
+
+                val end = Offset(
+                    x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
+                    y = (circleRadius * sin(angleInRad) + lineLength + circleCenter.y).toFloat()
+                )
+                rotate(
+                    angleInDegrees + 180,
+                    pivot = start
+                ) {
+                    drawLine(
+                        color = gray,
+                        start = start,
+                        end = end,
+                        strokeWidth = lineThickness.dp.toPx()
+                    )
+                }
+            }
+
+            val clockHands = listOf(ClockHands.Seconds, ClockHands.Minutes, ClockHands.Hours)
+
+            clockHands.forEach { clockHand ->
+                val angleInDegrees = when (clockHand) {
+                    ClockHands.Seconds -> {
+                        seconds * 360f / 60f
+                    }
+
+                    ClockHands.Minutes -> {
+                        (minutes + seconds / 60f) * 360f / 60f
+                    }
+
+                    ClockHands.Hours -> {
+                        (((hours % 12) / 12f * 60f) + minutes / 12f) * 360f / 60f
                     }
                 }
-
-                val clockHands = listOf(ClockHands.Seconds, ClockHands.Minutes, ClockHands.Hours)
-
-                clockHands.forEach { clockHand ->
-                    val angleInDegrees = when (clockHand) {
-                        ClockHands.Seconds -> {
-                            seconds * 360f / 60f
-                        }
-
-                        ClockHands.Minutes -> {
-                            (minutes + seconds / 60f) * 360f / 60f
-                        }
-
-                        ClockHands.Hours -> {
-                            (((hours % 12) / 12f * 60f) + minutes / 12f) * 360f / 60f
-                        }
+                //clock hands sizes
+                val lineLength = when (clockHand) {
+                    ClockHands.Seconds -> {
+                        circleRadius * 0.9f
                     }
-                    //clock hands sizes
-                    val lineLength = when (clockHand) {
-                        ClockHands.Seconds -> {
-                            circleRadius * 0.9f
-                        }
 
-                        ClockHands.Minutes -> {
-                            circleRadius * 0.8f
-                        }
-
-                        ClockHands.Hours -> {
-                            circleRadius * 0.5f
-                        }
+                    ClockHands.Minutes -> {
+                        circleRadius * 0.8f
                     }
-                    val lineThickness = when (clockHand) {
-                        ClockHands.Seconds -> {
-                            3f
-                        }
 
-                        ClockHands.Minutes -> {
-                            5f
-                        }
-
-                        ClockHands.Hours -> {
-                            5f
-                        }
-                    }
-                    val start = Offset(
-                        x = circleCenter.x,
-                        y = circleCenter.y
-                    )
-
-                    val end = Offset(
-                        x = circleCenter.x,
-                        y = lineLength + circleCenter.y
-                    )
-                    rotate(
-                        angleInDegrees - 180,
-                        pivot = start
-                    ) {
-                        drawLine(
-                            color = if (clockHand == ClockHands.Seconds) redOrange else gray,
-                            start = start,
-                            end = end,
-                            strokeWidth = lineThickness.dp.toPx()
-                        )
+                    ClockHands.Hours -> {
+                        circleRadius * 0.5f
                     }
                 }
-           // }
+                val lineThickness = when (clockHand) {
+                    ClockHands.Seconds -> {
+                        3f
+                    }
+
+                    ClockHands.Minutes -> {
+                        5f
+                    }
+
+                    ClockHands.Hours -> {
+                        5f
+                    }
+                }
+                val start = Offset(
+                    x = circleCenter.x,
+                    y = circleCenter.y
+                )
+
+                val end = Offset(
+                    x = circleCenter.x,
+                    y = lineLength + circleCenter.y
+                )
+                rotate(
+                    angleInDegrees - 180,
+                    pivot = start
+                ) {
+                    drawLine(
+                        color = if (clockHand == ClockHands.Seconds) redOrange else gray,
+                        start = start,
+                        end = end,
+                        strokeWidth = lineThickness.dp.toPx()
+                    )
+                }
+            }
+            // }
         }
     }
 }
@@ -311,12 +307,4 @@ enum class ClockHands {
     Seconds,
     Minutes,
     Hours
-}
-
-// For displaying preview in
-// the Android Studio IDE emulator
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MainContent()
 }
