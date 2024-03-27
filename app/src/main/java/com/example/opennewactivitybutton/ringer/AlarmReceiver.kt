812dfb5fb9ef63dcdb7b7c0fad39e60e27b8f7ce
sl508 <steven.lang508@myci.csuci.edu>
2024-03-26 19:26:57 -0700

    saves progress for testing alarm to game

--- app/src/main/java/com/example/opennewactivitybutton/ringer/AlarmReceiver.kt
+++ app/src/main/java/com/example/opennewactivitybutton/ringer/AlarmReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.example.opennewactivitybutton.games.Stub
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -13,6 +14,22 @@ class AlarmReceiver : BroadcastReceiver() {
             val alarmRing = AlarmRing(it)
             alarmRing.playRingtone()
 
+            val stubIntent = Intent(it, Stub::class.java).apply {
+                // Required when starting an Activity from a BroadcastReceiver
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            it.startActivity(stubIntent)
+
+            /*
+            // instead of forcing a game open, throw the game links (like above) as a choice they can click
+            val gamesIntent = Intent(Intent.ACTION_SEND)
+            val chooser = Intent.createChooser(intent, "GAMES")
+            try {
+                it.startActivity(chooser)
+            } catch (e: ActivityNotFoundException) {
+                // Define what your app should do if no activity can handle the intent.
+            }
+            */
         }
     }
 }
